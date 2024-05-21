94b6fb17382a0f7c055a32cc66e5b4c69ad2705b
Stonelukas <lukasgattermann18@gmail.com>
2024-05-21 06:22:34 +0200

    gitsigns config

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,19 +1,19 @@
 return {
-	"catppuccin/nvim",
-	lazy = false,
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha",
-			transparent_background = false,
-			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-				comments = { "italic" }, -- Change the style of comments
-			},
-		})
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "mocha",
+            transparent_background = false,
+            styles = {       -- Handles the styles of general hi groups (see `:h highlight-args`):
+                comments = { "italic" }, -- Change the style of comments
+            },
+        })
 
-		vim.o.termguicolors = true
-		vim.o.background = "light"
-		vim.cmd.colorscheme("catppuccin-mocha")
-	end,
+        vim.o.termguicolors = true
+        vim.o.background = "light"
+        vim.cmd.colorscheme("catppuccin-mocha")
+    end,
 }
