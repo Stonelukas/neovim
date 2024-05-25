73df050e98033e86af452b1bccd7a59ec40be8ab
Stonelukas <lukasgattermann18@gmail.com>
2024-05-26 01:08:33 +0200

    lualine, sessions (26.05.24)

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,17 +1,17 @@
 return {
-	"rmagatti/auto-session",
-	config = function()
-		require("auto-session").setup({
-			auto_session_use_git_branch = true,
-			session_lens = {
-				buftypes_to_ignore = {},
-				load_on_setup = true,
-				theme_conf = { border = true },
-				previewer = true,
-			},
-		})
-		vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
-			noremap = true,
-		})
-	end,
+    "rmagatti/auto-session",
+    config = function()
+        require("auto-session").setup({
+            auto_session_use_git_branch = true,
+            session_lens = {
+                buftypes_to_ignore = {},
+                load_on_setup = true,
+                theme_conf = { border = true },
+                previewer = true,
+            },
+        })
+        vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
+            noremap = true,
+        })
+    end,
 }
