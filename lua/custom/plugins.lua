587c15d75da5b7591a670cc62ebe10e378def111
Stonelukas <lukasgattermann18@gmail.com>
2024-06-01 04:55:57 +0200

    (06/01/2024)

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -10,7 +10,6 @@ return {
 			--  - yinq - [Y]ank [I]nside [N]ext [']quote
 			--  - ci'  - [C]hange [I]nside [']quote
 			require("mini.ai").setup({ n_lines = 500 })
-
 			-- Add/delete/replace surroundings (brackets, quotes, etc.)
 			--
 			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
@@ -20,7 +19,7 @@ return {
 		end,
 	},
 	{
-		"chaoren/vim-wordmotion",
+		"kckhaoren/vim-wordmotion",
 	},
 	{
 		"anuvyklack/vim-smartword",
@@ -29,7 +28,103 @@ return {
 		"mbbill/undotree",
 	},
 	{
-
 		"jlanzarotta/bufexplorer",
+	},
+	{
+		"tpope/vim-surround",
+		config = function()
+			vim.g.surround_no_maKppings = 1
+		end,
+	},
+	{
+		"tpope/vim-repeat",
+		config = function()
+			vim.keymap.set("n", "gq", "gqap")
+		end,
+	},
+	{
+		"tpope/vim-speeddating",
+		config = function() end,
+		vim.keymap.set("n", "<leader>dd", "<Plug>SpeedDatingdate"),
+	},
+	{
+		"tpope/vim-unimpaired",
+	},
+	{
+		"svermeulen/vim-easyclip",
+		config = function()
+			vim.g.EasyClipUseSubstituteDefaults = 1
+			vim.g.EasyClipAlwaysMoveCursorToEndOfPaste = 1
+			vim.g.EasyClipAutoFormat = 1
+			vim.keymap.set("n", "<leader>yf", "<Cmd>call EasyClip#Yank(expand('%'))<CR>")
+			vim.keymap.set("n", "<leader>cf", "<plug>EasyClipToggleFormattedPaste")
+			vim.keymap.set("n", "<leader>cc", "<Plug>EasyClipPasteAlternative")
+			vim.keymap.set("n", "<leader>ch", "<Plug>EasyClipPasteBefore")
+		end,
+	},
+	{
+		"svermeulen/vim-subversive",
+		config = function()
+			vim.keymap.set("n", "s", "<plug>SubversiveSubstitute")
+			vim.keymap.set("n", "ss", "<plug>SubversiveSubstituteLine")
+			vim.keymap.set("n", "S", "<plug>SubversiveSubstituteToEndOfLine")
+			vim.cmd([[ onoremap ie :exec "normal! ggVG"<cr> ]])
+			vim.cmd([[ onoremap iv :exec "normal! HVL"<cr> ]])
+			vim.keymap.set({ "n", "x" }, "<leader>s", "<plug>(SubversiveSubstituteRange)")
+			vim.keymap.set("n", "<leader>ss", "<plug>(SubversiveSubstituteWordRange)")
+		end,
+	},
+	{
+		"glts/vim-magnum",
+	},
+	{
+		"glts/vim-radical",
+		config = function() end,
+	},
+	{
+		"xiyaowong/transparent.nvim",
+		config = function()
+			require("transparent").setup({
+				extra_groups = {
+					"NvimTreeNormal",
+					"NormalNC",
+					"LspInfoFloat",
+					"TelescopeNormal",
+					"TelescopeBorder",
+					"TelescopePromptBorder",
+					"TelescopeResultsBorder",
+					"TelescopePreviewBorder",
+					"TelescopePromptNormal",
+					"TelescopeResultsNormal",
+					"TelescopePreviewNormal",
+					"BufferLineFill",
+					"BufferLineBackground",
+					"BufferLineCurrent",
+					"BufferLineCurrentVisible",
+					"BufferLineBufferSelected",
+					"BufferLineFill",
+					"Pmenu",
+					"PmenuSel",
+					"PmenuSbar",
+					"PmenuThumb",
+					"GitSignsAdd",
+					"GitSignsChange",
+					"GitSignsDelete",
+					"GitSignsAddLn",
+					"GitSignsChangeLn",
+					"GitSignsDeleteLn",
+					"DiffAdd",
+					"DiffChange",
+					"DiffDelete",
+					"DiffText",
+					"DiffAdded",
+					"DiffFile",
+					"DiffNewFile",
+					"DiffLine",
+					"DiffRemoved",
+				},
+			})
+			require("tokyonight").setup({ transparent = vim.g.transparent_enabled })
+		end,
 	},
 }
