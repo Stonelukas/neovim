5e06fb491e4f4bdf136d314dcca3ec3417268d9d
stonelukas <lukasgattermann18@gmail.com>
2024-10-05 08:53:33 +0200

    Auto save plugin

---
+++ lua/plugins/autosave.lua
@@ -0,0 +1,38 @@
+return {
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle",
+    event = { "InsertLeave", "TextChanged" },
+    keys = {
+        { "<leader>as", "<cmd>ASToggle<cr>", desc = "AutoSave" },
+    },
+    opts = {
+        enabled = true,
+        execution_message = {
+            enabled = true,
+            message = function()
+                local filename = vim.fn.expand("%:t")
+                return 'AutoSave: saved "' .. filename .. '" at ' .. vim.fn.strftime("%H:%M:%S")
+            end,
+        },
+        trigger_events = {                                 -- See :h events
+            immediate_save = { "BufLeave", "FocusLost" },  -- vim events that trigger an immediate save
+
+            defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+            cancel_defered_save = { "InsertEnter" },       -- vim events that cancel a pending deferred save
+        },
+        conditions = function(buf)
+            local fn = vim.fn
+
+            -- don't save for special-buffers
+            if fn.getbufvar(buf, "buftype") ~= "" then
+                return false
+            end
+            return true
+        end,
+        noautocmd = false,
+        lockmarks = false,
+
+        write_all_buffers = false,
+        debounce_delay = 3000,
+    },
+}
